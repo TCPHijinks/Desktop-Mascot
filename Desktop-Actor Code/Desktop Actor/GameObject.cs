0c3cf3e186a5590a76d9e7eae9ff1d474285fec1
PhylacteryLost <chodge.iam@gmail.com>
2020-02-13 11:32:49 +1000

    Fixed play area boundary constraints
    GameObjects are now given a "play area" instead of four boundaries they
    can't collide through. This fixes collision detection issues by
    essentially having whitelisted cordinates it must be inside. This
    prevents the GameObject escaping the play area by traveling too fast for the blacklisted cordinates to be checked.

--- Desktop-Actor Code/Desktop Actor/GameObject.cs
+++ Desktop-Actor Code/Desktop Actor/GameObject.cs
@@ -10,54 +10,54 @@ namespace Desktop_Actor
 {
     public class GameObject
     {
-        List<Rectangle> boundaries;
+        private readonly Rectangle _playArea;
         public Point Position;
-        public Dimensions Dimension;
+        public Dimensions MyDimensions;
         public bool CursorDragging { private set; get; }
 
-        public GameObject(Control form, List<Rectangle> boundaries)
+        public GameObject(Control form, Rectangle playAreaBoundary)
         {
-            this.boundaries = boundaries;
+            this._playArea = playAreaBoundary;
 
             Position = new Point();
-            Dimension = new Dimensions();
+            MyDimensions = new Dimensions();
 
             // Subscribe to key press events.
             form.MouseDown += MouseClick;
             form.MouseUp += MouseUp;
 
             // Starting position.
-            Dimension.Width = form.Width / 2;
-            Dimension.Height = form.Height / 3;
+            MyDimensions.Width = form.Width / 2;
+            MyDimensions.Height = form.Height / 3;
         }
 
-
-        public void Inside()
+        /// <summary>
+        /// Keeps game object inside of the play area's boundaries.
+        /// </summary>
+        public void KeepInsidePlayArea()
         {
-           
-            foreach (Rectangle boundary in boundaries)
-            {      
-                if (boundary.Contains(Top() ))
-                {
-                    Position.Y = boundary.Y + boundary.Height;
-                }
-                if (boundary.Contains(Bottom() ))
-                {
-                    Position.Y = boundary.Y - Dimension.Height;
-                }
-                if (boundary.Contains(Left() ))
-                {
-                    Position.X = boundary.X + boundary.Width;
-                }
-                if (boundary.Contains(Right() ))
-                {
-                    Position.X = boundary.X - Dimension.Width;
-                }
-
+            // If top of GameObject is higher than top of the boundary.
+            if (Position.Y < _playArea.Y)
+            {
+                Position.Y = _playArea.Y;
             }
-           
-        }
+            // If bottom of GameObject is lower than bottom of the boundary.
+            else if ((Position.Y + MyDimensions.Height) > (_playArea.Y + _playArea.Height))
+            {
+                Position.Y = _playArea.Y + (_playArea.Height - MyDimensions.Height);
+            }
 
+            // If left of GameObject is outside of boundary's left side.
+            if (Position.X < _playArea.X)
+            {
+                Position.X = _playArea.X;
+            }
+            // If right of GameObject is outside of boundary's right side.
+            else if ((Position.X + MyDimensions.Width) > (_playArea.X + _playArea.Width))
+            {
+                Position.X = (_playArea.X + _playArea.Width) - MyDimensions.Width;
+            }
+        }
 
         #region Event Methods
 
@@ -75,35 +75,29 @@ namespace Desktop_Actor
 
         #endregion Event Methods
 
-
-
         /// <summary>
         /// Returns point of left side at actor center height.
         /// </summary>
         /// <returns></returns>
         public Point Top()
         {
-            return new Point(Position.X + Dimension.Width/2, Position.Y);
+            return new Point(Position.X + MyDimensions.Width / 2, Position.Y);
         }
 
         public Point Bottom()
         {
-            return new Point(Position.X + Dimension.Width/2, Position.Y + Dimension.Height);
+            return new Point(Position.X + MyDimensions.Width / 2, Position.Y + MyDimensions.Height);
         }
 
         public Point Left()
         {
-            return new Point(Position.X, Position.Y + Dimension.Height / 2);
+            return new Point(Position.X, Position.Y + MyDimensions.Height / 2);
         }
 
         public Point Right()
         {
-            return new Point(Position.X + Dimension.Width, Position.Y + Dimension.Height / 2);
+            return new Point(Position.X + MyDimensions.Width, Position.Y + MyDimensions.Height / 2);
         }
-
-
-       
-
 
         public struct Dimensions
         {
